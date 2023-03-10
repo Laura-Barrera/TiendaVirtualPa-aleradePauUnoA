4ee92ac743b51d48e178a4c9ae659408e6499748
ItsAiz <andres.sierra03@uptc.edu.co>
2023-03-10 10:24:50 -0500

    unused style removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,6 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/globalStyles.css') }}" rel="stylesheet">
     <link href="{{ asset('css/dashboardStyles.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/clockpicker.css') }}" rel="stylesheet">
 </head>
 <body id="body" class="body_move">
     <div id="app">
@@ -134,7 +133,6 @@
     <!-- Scripts -->
     <script src="{{asset('js/app.js')}}"></script>
     <script src="{{asset('js/dashboardScript.js')}}"></script>
-    <script src="{{asset('js/clockpicker.js')}}"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
