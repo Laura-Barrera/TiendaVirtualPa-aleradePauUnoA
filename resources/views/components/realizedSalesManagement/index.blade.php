2038b4e5a8406df0a560bdb8a2e4d103965939d9
Cristian66333 <74688238+Cristian66333@users.noreply.github.com>
2023-03-23 14:31:29 -0500

    Update index.blade.php

--- resources/views/components/realizedSalesManagement/index.blade.php
+++ resources/views/components/realizedSalesManagement/index.blade.php
@@ -13,11 +13,12 @@
                     <td width="100"><b>FECHA</b></td>
                     <td width="100">
                         <input class="form-control" type="date" max="<?php echo date("Y-m-d"); ?>"
-                               value="" name="" required>
+                               value="" name="" id="datePicker" onchange="getDate();" required>
                     </td>
                 </tr>
             </table>
             </p>
+
 
             <table class="table table-striped table-bordered border-dark productsTable" style="width: 100%;">
 
@@ -37,21 +38,8 @@
 
                 </thead>
 
-                <tbody class="text-center" style="background-color: #F9F9F9">
+                <tbody class="text-center" style="background-color: #F9F9F9" id="table">
 
-                @foreach ($shipping_order as $shipping)
-                    <tr >
-                        <td>{{$shipping->id}}</td>
-                        <td>{{$shipping->saleDate}}</td>
-                        <td>{{\App\Models\Customer::all()->find($shipping->id_customer)->documentNumber}}</td>
-                        <td>{{\App\Models\Customer::all()->find($shipping->id_customer)->name}}</td>
-                        <td>{{\App\Models\ShippingOrder::all()->find($shipping->id_shipping_order)->address}}</td>
-                        <td>{{\App\Models\ShippingOrder::all()->find($shipping->id_shipping_order)->city}}</td>
-                        <td>{{\App\Models\ShippingOrder::all()->find($shipping->id_shipping_order)->department}}</td>
-                        <td>{{\App\Models\PaymentMethod::all()->find($shipping->id_payment_method)->nameMethod}}</td>
-                        <td>{{$shipping->totalCost}}</td>
-                    </tr>
-                @endforeach
 
                 </tbody>
 
@@ -59,5 +47,21 @@
 
         </div>
     </div>
+
+
+@endsection
+@section('js2')
+    <script>
+        var getDate = function () {
+            var date = document.getElementById('datePicker').value || ""
+            var table = document.getElementById('table')
+            msg = '';
+            @foreach ($shipping_order as $shipping)
+            (date === '{{$shipping->saleDate}}') ? (msg += '<tr> <td>{{$shipping->id}}</td> <td>{{$shipping->saleDate}}</td> <td>{{\App\Models\Customer::all()->find($shipping->id_customer)->documentNumber}}</td> <td>{{\App\Models\Customer::all()->find($shipping->id_customer)->name}}</td> <td>{{\App\Models\ShippingOrder::all()->find($shipping->id_shipping_order)->address}}</td> <td>{{\App\Models\ShippingOrder::all()->find($shipping->id_shipping_order)->city}}</td> <td>{{\App\Models\ShippingOrder::all()->find($shipping->id_shipping_order)->department}}</td> <td>{{\App\Models\PaymentMethod::all()->find($shipping->id_payment_method)->nameMethod}}</td> <td>{{$shipping->totalCost}}</td> </tr>' ): '';
+            @endforeach
+                table.innerHTML=msg;
+        }
+
+    </script>
 
 @endsection
