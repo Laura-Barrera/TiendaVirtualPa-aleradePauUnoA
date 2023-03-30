8175f311186f2f1e3643f3d3317264ec1187a8dd
StevenRincon24 <74875614+StevenRincon24@users.noreply.github.com>
2023-03-30 17:14:23 -0500

    Product Table Search

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -180,6 +180,16 @@
     <script src="{{asset('js/dashboardScript.js')}}"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+
+
+
+    <link rel="stylesheet" href= "https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js"> </script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"> </script>
+    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"> </script>
+
     <script>
         window.addEventListener("pageshow", function (event) {
             let historyTraversal = event.persisted ||
